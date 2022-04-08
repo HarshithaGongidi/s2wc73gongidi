38efac5ded7735f2c8938098d2bfbda4c1bac6c8
harshitha gongidi <g9harshitha@gmail.com>
2022-04-07 23:34:48 -0500

    bonus

--- routes/Rabbit.js
+++ routes/Rabbit.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET RABBIT. */
 router.get('/', function(req, res, next) {
-    res.render('rabbit', { title: "Search Results"});
+  res.render('rabbit', { title: "Search Results"});
 });
 
 module.exports = router;
