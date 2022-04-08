b81824cf3d93eb4f05aa0032835cb2975d41ed70
harshitha gongidi <g9harshitha@gmail.com>
2022-04-07 21:20:03 -0500

    rabbit

--- routes/Rabbit.js
+++ routes/Rabbit.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/Rabbit', function(req, res, next) {
+/* GET RABBIT. */
+router.get('/', function(req, res, next) {
   res.render('Rabbit', { title: 'Search Results - Rabbit' });
 });
 
