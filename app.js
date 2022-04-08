ed234944c304cd0d2cbc17a264abb604859561ff
harshitha gongidi <g9harshitha@gmail.com>
2022-04-07 23:20:12 -0500

    images

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var RabbitRouter = require('./routes/Rabbit');
 var AddmodsRouter = require('./routes/Rabbit');
+var SelectorRouter = require('./routes/Selector');
 var app = express();
 
 // view engine setup
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Rabbit', RabbitRouter);
 app.use('/Addmods', AddmodsRouter);
+app.use('/Selector', SelectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
